8aeb12e1a12534742b2b76a3b5a3e7e75ffc6701
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-02-20 19:19:32 -0500

    Create main-view.jsx
    MainView component created, includes 3 movies in array

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "Bridesmaids",
+      image: "",
+      director: "",
+    },
+    {
+      id: 2,
+      title: "",
+      image: "",
+      director: "",
+    },
+    {
+      id: 3,
+      title: "",
+      image: "",
+      director: "",
+    },
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+/* Write the MainView component with at least 3 movies in the movies array state
+ */
