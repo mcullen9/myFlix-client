29b7b27604dba4ca9cddc0beb9a906c29ca81b45
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-03-08 03:21:56 -0500

    removed onMovieClick
    Wrapped Card in <Link /> react-router-dom component and removed onMovieClick button

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,23 +2,24 @@ import "./movie-card.scss";
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card className="h-100" onClick={() => onMovieClick(movie)}>
-      <Card.Img variant="top" src={movie.ImagePath} />
-      <Card.Body>
-        <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Description}</Card.Text>
-        <Button
-          onClick={() => onMovieClick(movie)}
-          className="info-button"
-          variant="link"
-        >
-          More Info
-        </Button>
-      </Card.Body>
-    </Card>
+    <>
+      <Link
+        className="link-card"
+        to={`/movies/${encodeURIComponent(movie._id)}`}
+      >
+        <Card>
+          <Card.Img variant="top" src={movie.ImagePath} />
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>{movie.Description}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Link>
+    </>
   );
 };
 
@@ -29,5 +30,4 @@ MovieCard.propTypes = {
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
